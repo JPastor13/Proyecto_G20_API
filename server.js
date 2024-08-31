341943338d457ce100c00d5fbf8a986880609398
JPastor13 <Richard.pastor.sanchez@gmail.com>
2024-08-31 05:20:30 -0500

    jpastor_31082024_0039

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-
-const jsonServer = require("json-server")
+ 
+const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router("db.json")
+const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-
-server.use(jsonServer.rewriter({
-    "/*": "/$1",
-}))
 server.use(router)
-server.listen(3000, () => {
-    console.log("JSON Server is running")
-})
 
-// Export the Server API
-module.exports = server
+const port = process.env.PORT || 3006
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
